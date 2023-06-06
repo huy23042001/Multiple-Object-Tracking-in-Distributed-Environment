f95cc468181dfa712be847fefc557a483875f592
Yuh <hataihuy2001@gmail.com>
2023-06-07 00:07:33 +0700

    update sql

--- WebUI/sqlquery.py
+++ WebUI/sqlquery.py
@@ -8,7 +8,7 @@ class MySQLBuilder:
         #     password="password",
         #     database="test") 
         self.mydb = connect(
-            host="192.168.100.126",
+            host="localhost",
             user="hduser",
             password="hduser@123",
             database="object_tracking") 
